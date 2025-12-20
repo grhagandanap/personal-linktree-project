da92da672b299cae6be4a9b093b29e6a6c47a6f3
Grha Gandana P <grhagandanap@gmail.com>
2025-12-21 01:59:42 +0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+const typed = new Typed("#typed", {
+    strings: ["Web Developer", "Petroleum Engineer", "Data Analyst"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
+})
