eaaaa447673ac8b2dabe833e665e9d90f923bb64
Grha Gandana P <grhagandanap@gmail.com>
2025-12-25 09:36:20 +0700

    Update 25-12-2025

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,24 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+
+  server: {
+    watch: {
+      // Prevent watching heavy folders
+      ignored: [
+        '**/node_modules/**',
+        '**/.git/**',
+        '**/dist/**',
+        '**/.wrangler/**',
+      ],
+    },
+    hmr: {
+      overlay: false, // reduce browser work
+    },
+  },
+
+  build: {
+    sourcemap: false, // dev build lighter
+  },
 })
